66c7e2970209249c58b2fa280427efeee978a459
abdallahawad3 <djdgdnnxcbj@gmail.com>
2024-05-29 22:40:55 +0300

    Finsh ✅

--- src/components/ui/SelectMenu.tsx
+++ src/components/ui/SelectMenu.tsx
@@ -42,7 +42,7 @@ const people = [
   },
   {
     id: 6,
-    name: "Hellen Schmidt",
+    name: "Hellen Schmiddt",
     avatar:
       "https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
   },
